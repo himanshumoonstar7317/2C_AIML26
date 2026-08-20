7da54483554611aabfbde566db836f6611e86148
Himanshu Gupta <himanshuguptaheer7317@gmail.com>
2026-08-20 16:11:45 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,18 @@
+let student= new Object();
+student.Name= "Himanshu";
+student.Age= 20;
+student.City= "Delhi";
+student.Learning= function() {
+    console.log("JavaScript");
+};
+console.log(student.Name);
+student.Learning();
+ 
+let employee= {
+    name: "Himanshu",
+    id: 83,
+    city: "Ghaziabad",
+    learning: function() {
+        console.log("JavaScript");
+}
+}
