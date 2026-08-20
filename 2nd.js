7c14290d4b95d3a468134190bd40e3ec4f4d6d6f
Himanshu Gupta <himanshuguptaheer7317@gmail.com>
2026-08-20 16:10:43 +0530

    Add files via upload

---
+++ 2nd.js
@@ -0,0 +1,8 @@
+let price= 26;
+let quantity= 10;
+let total= price*quantity;
+let codeisfun= true;
+console.log("Price: " + price);
+console.log("Quantity: " + quantity);
+console.log("Total: " + total);
+console.log("code is fun: " + codeisfun);
